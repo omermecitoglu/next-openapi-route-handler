33c23a53ebf212f71d4e1852ca7708c8b0224891
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-06-10 15:32:43 +0700

    add string to boolean conversion in error-handler

--- src/core/zod-error-handler.ts
+++ src/core/zod-error-handler.ts
@@ -6,12 +6,27 @@ function convertStringToNumber(input: Record<string, unknown>, keys: string[]) {
   }, input);
 }
 
+function getTrueBoolean(input: string) {
+  if (input === "true") return true;
+  if (input === "false") return false;
+  return null;
+}
+
+function convertStringToBoolean(input: Record<string, unknown>, keys: string[]) {
+  return keys.reduce((mutation, key) => {
+    return { ...mutation, [key]: getTrueBoolean(mutation[key] as string) } as Record<string, unknown>;
+  }, input);
+}
+
 export function safeParse<T>(schema: ZodType<T>, input: Record<string, unknown>) {
   const result = schema.safeParse(input);
   if (!result.success) {
     for (const issue of result.error.issues) {
       if (issue.code === "invalid_type" && issue.expected === "number" && issue.received === "string") {
         return safeParse(schema, convertStringToNumber(input, issue.path as string[]));
+      }
+      if (issue.code === "invalid_type" && issue.expected === "boolean" && issue.received === "string") {
+        return safeParse(schema, convertStringToBoolean(input, issue.path as string[]));
       }
     }
     throw result.error;
