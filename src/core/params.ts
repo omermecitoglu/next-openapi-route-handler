bcbb88e664a1535a5f84e38a5549f5d90f44d430
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-09-26 14:11:25 +0700

    refactor: resolveParams

--- src/core/params.ts
+++ src/core/params.ts
@@ -4,17 +4,14 @@ import type { ZodType } from "zod";
 
 export function resolveParams(kind: ParameterObject["in"], source?: ZodType<unknown>) {
   if (!source) return [];
-  const collection: ParameterObject[] = [];
   const schema = convertToOpenAPI(source, false);
   if (!("properties" in schema)) throw new Error("Invalid object schema");
-  for (const [name, definition] of Object.entries(schema.properties)) {
-    collection.push({
-      in: kind,
-      name: name,
-      required: schema.required?.includes(name) ?? false,
-      description: definition.description,
-      schema: definition,
-    });
-  }
-  return collection;
+  const entries = Object.entries(schema.properties ?? {});
+  return entries.map<ParameterObject>(([name, definition]) => ({
+    in: kind,
+    name: name,
+    required: schema.required?.includes(name) ?? false,
+    description: definition.description,
+    schema: definition,
+  }));
 }
