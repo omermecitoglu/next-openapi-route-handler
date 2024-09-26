2d952cfae191fbf9aba50a20b2efedb5fe378d8a
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-09-26 13:05:34 +0700

    test: resolveContent

---
+++ src/core/content.test.ts
@@ -0,0 +1,68 @@
+import { describe, expect, it } from "@jest/globals";
+import { z } from "zod";
+import { resolveContent } from "./content";
+import type { RequestBodyObject } from "@omer-x/openapi-types/request-body";
+
+describe("resolveContent", () => {
+  it("should return undefined if source is not provided", () => {
+    const result = resolveContent(undefined);
+    expect(result).toBeUndefined();
+  });
+
+  it("should create content for a string schema reference", () => {
+    const result = resolveContent("User", false);
+
+    const expectedContent = {
+      "application/json": {
+        schema: {
+          $ref: "#/components/schemas/User",
+        },
+      },
+    } as RequestBodyObject["content"];
+
+    expect(result).toEqual(expectedContent);
+  });
+
+  it("should create content for a Zod schema", () => {
+    const zodSchema = z.object({
+      name: z.string(),
+      age: z.number(),
+    });
+
+    const result = resolveContent(zodSchema, false);
+
+    const expectedContent = {
+      "application/json": {
+        schema: {
+          type: "object",
+          properties: {
+            name: {
+              type: "string",
+            },
+            age: {
+              type: "number",
+            },
+          },
+          required: ["name", "age"],
+          additionalProperties: false,
+        },
+      },
+    } satisfies RequestBodyObject["content"];
+
+    expect(result).toEqual(expectedContent);
+  });
+
+  it("should create multipart/form-data content when isFormData is true", () => {
+    const result = resolveContent("User", false, true);
+
+    const expectedContent = {
+      "multipart/form-data": {
+        schema: {
+          $ref: "#/components/schemas/User",
+        },
+      },
+    } satisfies RequestBodyObject["content"];
+
+    expect(result).toEqual(expectedContent);
+  });
+});
