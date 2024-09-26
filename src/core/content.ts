7363389932acdb1f096ad753f4fd1565447974bd
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-09-26 12:47:22 +0700

    refactor: resolveContent

--- src/core/content.ts
+++ src/core/content.ts
@@ -1,21 +1,16 @@
 import { createSchemaRef } from "~/utils/openapi-components";
 import convertToOpenAPI from "./zod-to-openapi";
 import type { RequestBodyObject } from "@omer-x/openapi-types/request-body";
-import type { SchemaObject } from "@omer-x/openapi-types/schema";
 import type { ZodType } from "zod";
-
-function resolveSchema(source: ZodType<unknown> | string, isArray: boolean = false): SchemaObject {
-  if (typeof source === "string") {
-    return createSchemaRef(source, isArray);
-  }
-  return convertToOpenAPI(source, isArray);
-}
 
 export function resolveContent(source?: ZodType<unknown> | string, isArray: boolean = false, isFormData: boolean = false) {
   if (!source) return undefined;
+
+  const schema = typeof source === "string" ? createSchemaRef(source, isArray) : convertToOpenAPI(source, isArray);
+
   return {
     [isFormData ? "multipart/form-data" : "application/json"]: {
-      schema: resolveSchema(source, isArray),
+      schema,
     },
   } as RequestBodyObject["content"];
 }
