13810156cfa892e4b9d402f7c37148e969a88d77
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-10-16 11:23:16 +0700

    refactor: add response content for 400

--- src/core/responses.ts
+++ src/core/responses.ts
@@ -1,10 +1,31 @@
+import z from "zod";
 import type { ResponseDefinition } from "~/types/response";
 import { resolveContent } from "./content";
+import convertToOpenAPI from "./zod-to-openapi";
 import type { ResponseObject, ResponsesObject } from "@omer-x/openapi-types/response";
 
 export function addBadRequest(queryParams?: unknown, requestBody?: unknown) {
   if (!queryParams && !requestBody) return undefined;
-  return { description: "Bad Request" } as ResponseObject;
+  return {
+    description: "Bad Request",
+    content: {
+      "application/json": {
+        schema: convertToOpenAPI(z.object({
+          success: z.boolean(),
+          errorType: z.enum([
+            "PARSE_FORM_DATA_ERROR",
+            "PARSE_REQUEST_BODY_ERROR",
+            "PARSE_SEARCH_PARAMS_ERROR",
+          ]),
+          zodIssues: z.object({
+            code: z.string(),
+            path: z.array(z.union([z.string(), z.number()])),
+            message: z.string(),
+          }).array(),
+        }), false),
+      },
+    },
+  } as ResponseObject;
 }
 
 export function bundleResponses(collection: Record<string, ResponseDefinition>) {
