8b30f73eda37548af6fbe2598469c79613f389ed
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-11-12 10:19:23 +0700

    refactor: createMediaType with examples

--- src/core/createMediaType.ts
+++ src/core/createMediaType.ts
@@ -2,11 +2,18 @@ import type { Entry } from "~/types/entry";
 import type { MediaTypeObject } from "@omer-x/openapi-types/media-type";
 import type { SchemaObject } from "@omer-x/openapi-types/schema";
 
-export default function createMediaType(schema: SchemaObject, example?: MediaTypeObject["example"]) {
+export default function createMediaType(
+  schema: SchemaObject,
+  example?: MediaTypeObject["example"],
+  examples?: MediaTypeObject["examples"],
+) {
   const mediaTypeEntries = [] as Entry<MediaTypeObject>[];
   mediaTypeEntries.push(["schema", schema]);
   if (example) {
     mediaTypeEntries.push(["example", example]);
+  }
+  if (examples) {
+    mediaTypeEntries.push(["examples", examples]);
   }
   return Object.fromEntries(mediaTypeEntries) as MediaTypeObject;
 }
