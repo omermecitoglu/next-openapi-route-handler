e165924d0557724ddb7cf76e090f995a769a1327
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-11-12 10:21:44 +0700

    refactor: resolveContent with examples

--- src/core/content.ts
+++ src/core/content.ts
@@ -1,20 +1,22 @@
 import { createSchemaRef } from "~/utils/openapi-components";
 import createMediaType from "./createMediaType";
 import convertToOpenAPI from "./zod-to-openapi";
+import type { ExampleObject } from "@omer-x/openapi-types/example";
 import type { RequestBodyObject } from "@omer-x/openapi-types/request-body";
-import type { ZodType } from "zod";
+import type { ZodType, ZodTypeDef } from "zod";
 
-export function resolveContent(
-  source?: ZodType<unknown> | string,
+export function resolveContent<I, O>(
+  source?: ZodType<O, ZodTypeDef, I> | string,
   isArray: boolean = false,
   isFormData: boolean = false,
-  customExample?: unknown,
+  customExample?: NoInfer<O>,
+  customExamples?: Record<string, ExampleObject<NoInfer<O>>>,
 ) {
   if (!source) return undefined;
 
   const schema = typeof source === "string" ? createSchemaRef(source, isArray) : convertToOpenAPI(source, isArray);
 
   return {
-    [isFormData ? "multipart/form-data" : "application/json"]: createMediaType(schema, customExample),
+    [isFormData ? "multipart/form-data" : "application/json"]: createMediaType(schema, customExample, customExamples),
   } as RequestBodyObject["content"];
 }
