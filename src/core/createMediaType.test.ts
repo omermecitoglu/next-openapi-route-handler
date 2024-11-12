cbd5bb93738e40b5a313909f17f8ef0dad5f2bd2
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-11-12 10:19:55 +0700

    test: createMediaType examples parameter

--- src/core/createMediaType.test.ts
+++ src/core/createMediaType.test.ts
@@ -5,6 +5,20 @@ import type { SchemaObject } from "@omer-x/openapi-types/schema";
 describe("createMediaType", () => {
   const mockSchema: SchemaObject = { type: "string" };
   const mockExample = "example text";
+  const mockExamples = {
+    "Example-1": {
+      value: "Example 1",
+      description: "1st Example",
+    },
+    "Example-2": {
+      value: "Example 2",
+      description: "2nd Example",
+    },
+    "Example-3": {
+      value: "Example 3",
+      description: "3rd Example",
+    },
+  };
 
   it("should create a MediaTypeObject with only the schema", () => {
     expect(createMediaType(mockSchema)).toStrictEqual({
@@ -16,6 +30,26 @@ describe("createMediaType", () => {
     expect(createMediaType(mockSchema, mockExample)).toStrictEqual({
       schema: mockSchema,
       example: mockExample,
+    });
+  });
+
+  it("should create a MediaTypeObject with schema and example when example is provided", () => {
+    expect(createMediaType(mockSchema, undefined, mockExamples)).toStrictEqual({
+      schema: mockSchema,
+      examples: {
+        "Example-1": {
+          value: "Example 1",
+          description: "1st Example",
+        },
+        "Example-2": {
+          value: "Example 2",
+          description: "2nd Example",
+        },
+        "Example-3": {
+          value: "Example 3",
+          description: "3rd Example",
+        },
+      },
     });
   });
 });
